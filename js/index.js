4e3b5f0180cf33e737ca487d2b30539e68d02121
Steven Fulakeza <steven.fulakeza@lehman.cuny.edu>
2021-06-03 12:59:48 -0400

    Created some JS examples for DOM manipulation

---
+++ js/index.js
@@ -0,0 +1,162 @@
+
+
+/* rest and spread operators
+
+rest (...)
+spread (...)
+
+
+rest
+
+
+
+*/
+
+// the following function multiplies numbers passed in the parameter
+//Rest
+function multiply(parameter, anotherParamter, ...args){ // rest operator
+
+
+    //name2 = "Mary Doe";
+//console.log(name2);
+    //body
+    console.log(args);
+    let product = 1;
+    for(let i in args){
+        product = product * args[i];
+        //let test ="";
+        //var test = "";
+    }
+
+    console.log(product);
+
+   /* for (let index = 0; index < args.length; index++){
+        console.log(args[index]);
+
+        product = product * args[i];
+    }
+    console.log(product);
+    */
+
+
+}
+
+
+
+
+
+
+
+multiply(2,4);
+multiply(1,2,3,4,5);
+
+
+/*function functionName(...argu){
+
+}*/
+
+
+/*Spread operator
+
+
+*/
+
+function sum(x, y, z){
+    return x + y + z;
+}
+
+const numbers = [10, 20, 30];
+
+console.log(sum(...numbers)); // Spread operator 10, 20, 30
+
+//console.log(sum(10, 20, 30));
+
+/*
+
+myFunction(...iterableObject)
+
+*/
+
+
+let t = document.getElementById("myDog");
+//console.log(t);
+
+let t1 = document.getElementsByClassName("dogs");
+console.log(t1);
+//console.log(Array.isArray(t1));
+
+let t2 = document.getElementsByTagName("h1");
+//console.log(t2);
+
+/* We can also be able to use querySelector, querySelectorAll
+
+Searching using CSS selectors
+1. Class seletor
+2. id selector
+3. element selector
+
+*/
+
+//The querySelector will return only the first element that matches the CSS selector(s)
+let t3 = document.querySelector(".dogs");
+//console.log(t3);
+
+// querySelectorAll. This returns all elements that matches a specified CSS selecor(s)
+
+let t4 = document.querySelectorAll(".dogs");
+
+/* The Array.from(object) returns an array objects from the given object
+*/
+let arrayT4 = Array.from(t4);
+//console.log(arrayT4);
+
+let arrayT4B = [...t4];
+//console.log(arrayT4B);
+
+let x = document.getElementById("mainBody").previousElementSibling;
+console.log(x)
+let x1 = document.getElementById("mainBody");
+x1.style.backgroundColor = 'yellow';
+
+let newParagraph = document.createElement("p");
+newParagraph.innerHTML = "This is our new paragraph";
+document.getElementById("mainBody").appendChild(newParagraph); //
+/*
+#mainBody{
+    background-color: yellow;
+}
+*/
+
+
+/*
+JS Events
+
+
+syntax for addEventListener
+document.addEventListener(event, function, useCapture);
+
+event: This specifies the name of the event. 
+
+function: This spefies the function that should when the event occurs.
+The first and second parameters are required. You must provide the event
+and the function.
+
+UseCapture: boolean value that specifies whether the event should be 
+executed in the capturing and the bubbling phase.
+
+*/
+
+
+document.getElementById("myDog").addEventListener("click", test );
+
+function test(){
+    alert("today");
+}
+
+document.getElementById("myFriend").addEventListener("click", function(){
+    test1("Scruffy");
+} );
+
+function test1(name1){
+    alert("today" + name1);
+}
